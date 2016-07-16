415fb80da87097ad9261d170269abc2618a7287a
李进德 <dddpeter@126.com>
2016-07-17 00:21:13 +0800

    特效添加

--- app.js
+++ app.js
@@ -1,73 +1,30 @@
 /**
  * Created by dddpe on 2016-7-15.
  */
-angular
-    .module('recruitApp',['ngRoute','ngAnimate','swipe'])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/home/home.html',
-                controller: 'homeControler',
-                controllerAs: 'home'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    })
-    .controller('homeControler',['$scope','$sce', function ($scope,$sce) {
-        $scope.musicOn = true;
-        $scope.pages = [
-            {url:'views/home/a.html',isShow:true}
-        ];
+$(document).ready(function(){
+    var audio = document.getElementById("backSound");
+    audio.pause();
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+        loop: false,
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素
+            swiperAnimate(swiper); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiper){
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }
+    });
+    $('.music').on('click',function(){
+        var audio = document.getElementById("backSound");
+        if(audio.paused) {
+            audio.play();
+            $('.music').addClass('music-circle');
+        }
+        else{
+            audio.pause();
+            $('.music').removeClass('music-circle')
+        }
+    });
 
-
-
-        $scope.next=function(index){
-            var length=$scope.pages.length;
-            if(index==length-1){
-                angular.forEach($scope.pages,function(v,i){
-                    if(i==0){
-                        v.isShow=true;
-                    }else{
-                        v.isShow=false;
-                    }
-                });
-            }
-            else{
-                angular.forEach($scope.pages,function(v,i){
-
-                    if(i==index+1){
-                        v.isShow=true;
-                    }else{
-                        v.isShow=false;
-                    }
-                });
-            }
-
-        };
-        $scope.pre=function(index){
-            var length=$scope.pages.length;
-            if(index==0){
-                angular.forEach($scope.pages,function(v,i){
-                    if(i==length-1){
-                        v.isShow=true;
-                    }else{
-                        v.isShow=false;
-                    }
-                });
-            }
-            else{
-                angular.forEach($scope.pages,function(v,i){
-
-                    if(i==index-1){
-                        v.isShow=true;
-                    }else{
-                        v.isShow=false;
-                    }
-                });
-
-            }
-        };
-
-
-    }]);
+});
