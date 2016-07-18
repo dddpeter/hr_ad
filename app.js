6e44e3ecdac65412c26421d9d9b0ac6b619c1a31
李进德 <dddpeter@126.com>
2016-07-18 08:58:14 +0800

    项目整理

---
+++ app.js
@@ -0,0 +1,39 @@
+/**
+ * Created by dddpe on 2016-7-15.
+ */
+$(document).ready(function(){
+    var audio = document.getElementById("backSound");
+    audio.pause();
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+        loop: false,
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素
+            swiperAnimate(swiper); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiper){
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }
+    });
+    $('.music').on('click',function(){
+        var audio = document.getElementById("backSound");
+        if(audio.paused) {
+            audio.play();
+            $('.music').addClass('music-circle');
+        }
+        else{
+            audio.pause();
+            $('.music').removeClass('music-circle')
+        }
+    });
+    $('#dataInfoDepart').on('click',function(){
+        var height=$('#dataInfoDepart').height();
+        $('.layer').css('display','block');
+        $('.layer').css('top','205%');
+    });
+    $('.closeLayer').children('i').on('click',function(){
+        $('.layer').css('display','none');
+        $('.layer').css('top','5%');
+    });
+
+});
